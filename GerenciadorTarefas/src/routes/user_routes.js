4c77afbf0f4d045a795b1318cd1e8d9cff6480e6
bidaaa <bidaviniciusribas@gmail.com>
2025-11-17 19:35:50 -0300

    Router working

--- GerenciadorTarefas/src/routes/user_routes.js
+++ GerenciadorTarefas/src/routes/user_routes.js
@@ -1,6 +1,8 @@
 import express from "express"
 import {register} from "../controllers/user_controller.js"
 
-export const router = express.Router()
+const router = express.Router()
 
 router.post('/register', register)
+
+export default router
