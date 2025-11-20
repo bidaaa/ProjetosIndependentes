14fe622c25265f02d7cc87b1eaa474a32d0586e1
bidaaa <bidaviniciusribas@gmail.com>
2025-11-20 04:07:25 -0300

    Adjusting dotenv, now working

--- GerenciadorTarefas/src/server.js
+++ GerenciadorTarefas/src/server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import app from './app.js';
 import connect from './config/db.js';
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const PORT = process.env.PORT;
 
